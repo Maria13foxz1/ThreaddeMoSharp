56c123a8b40db6dcf72043ec25f44a51900c0689
Maria13foxz1 <bezpalko.mariia1121@vu.cdu.edu.ua>
2024-02-26 19:54:28 +0200

    Add project files.

---
+++ ThreaddeMoSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace ThreaddeMoSharp
+{
+    internal class Program
+    {
+        public bool canStop = false;
+        public bool CanStop { get => canStop; }
+        static void Main(string[] args)
+        {
+            /*int numThreads = 3; // Кількість потоків для створення
+            int[] steps = { 1, 2, 3 }; // Кроки для кожного потоку
+            int[] threadWorkingTimes = { 10000, 12000, 9000 }; // Час в мілісекундах для кожного потоку
+            // Запускаємо керуючий потік
+            ControlThread controlThread = new ControlThread(numThreads);
+            Thread controller = new Thread(controlThread.Start);
+            controller.Start();
+            // Запускаємо робочі потоки
+            for (int i = 0; i < numThreads; i++)
+            {
+                WorkerThread workerThread = new WorkerThread(i + 1, steps[i], threadWorkingTimes[i], controlThread);
+                Thread worker = new Thread(workerThread.Start);
+                worker.Start();
+            }*/
+
+            (new Program()).Start();
+            Console.ReadKey();
+        }
+        void Start()
+        {
+            //Створюємо три потока
+            (new Thread(() => Calcuator(11, 1))).Start();
+            (new Thread(() => Calcuator(12, 2))).Start();
+            (new Thread(() => Calcuator(13, 3))).Start();
+            //Керуючий потік
+            Thread controller = new Thread(Stoper);
+            controller.Start();
+        }
+        void Calcuator(int id, int step)
+        {
+            long sum = 0;
+            int count = 0;
+            do
+            {
+                sum += step; // Додаємо крок до суми
+                count++;
+            } while (!canStop);
+
+            Console.WriteLine($"Thread {id}: Sum = {sum}, Count = {count}");
+        }
+        public void Stoper()
+        {
+            Thread.Sleep(5 * 1000);
+            canStop = true;
+        }
+    }
+}
+
---
+++ ThreaddeMoSharp/ThreadMonoCharp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreaddeMoSharp
+{
+    class ControlThread
+    {
+        private int numThreads; 
+        private bool[] threadFinished; 
+        private bool stop; 
+
+        // Конструктор класу
+        public ControlThread(int numThreads)
+        {
+            this.numThreads = numThreads;
+            threadFinished = new bool[numThreads];
+        }
+
+        // Метод для початку роботи керуючого потоку
+        public void Start()
+        {
+            // Чекаємо, поки всі потоки завершаться
+            while (true)
+            {
+                bool allFinished = true;
+                for (int i = 0; i < numThreads; i++)
+                {
+                    if (!threadFinished[i])
+                    {
+                        allFinished = false;
+                        break;
+                    }
+                }
+
+                if (allFinished)
+                {
+                    stop = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine("All threads finished their work.");
+        }
+
+        // Метод для встановлення прапорця завершення для конкретного потоку
+        public void SetThreadFinished(int threadIndex)
+        {
+            threadFinished[threadIndex] = true;
+        }
+
+        // Метод для перевірки, чи потрібно зупиняти роботу
+        public bool ShouldStop()
+        {
+            return stop;
+        }
+    }
+    // Клас робочого потоку
+    class WorkerThread
+    {
+        private int id; 
+        private int step; 
+        private int workingTimeMilliseconds;
+        private ControlThread controlThread; 
+    
+        public WorkerThread(int id, int step, int workingTimeMilliseconds, ControlThread controlThread)
+        {
+            this.id = id;
+            this.step = step;
+            this.workingTimeMilliseconds = workingTimeMilliseconds;
+            this.controlThread = controlThread;
+        }
+
+        public void Start()
+        {
+            double sum = 0; 
+            int count = 0;
+
+            DateTime startTime = DateTime.Now;
+            // Обчислення суми послідовності протягом вказаного часу
+            while ((DateTime.Now - startTime).TotalMilliseconds < workingTimeMilliseconds)
+            {
+                sum +=  step; 
+                count++;
+            }
+
+            // Виведення результатів
+            Console.WriteLine($"Thread {id}: Sum = {sum}, Elements = {count}");
+            controlThread.SetThreadFinished(id - 1); // Повідомляємо керуючому потоку, що цей потік завершив роботу
+        }
+    }
+}
